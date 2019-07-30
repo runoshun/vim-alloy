132af10702902ff6625e1f61529936a601951ee2
Sinan <shmorcel@wpi.edu>
2019-07-30 11:28:17 -0400

    add install.sh to accelerate and suggest the method of deployment.

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p ~/.vim/syntax ~/.vim/ftdetect ~/.vim/indent
+cp ftdetect/alloy.vim ~/.vim/ftdetect/alloy.vim
+cp syntax/alloy.vim ~/.vim/syntax/alloy.vim
+cp indent/alloy.vim ~/.vim/indent/alloy.vim
